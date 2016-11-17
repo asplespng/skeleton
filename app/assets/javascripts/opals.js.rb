732782b2398cd6b0e06aaec78ea1f5d2535a90dc
Per Evensen <per.evensen@gmail.com>
2016-11-17 09:18:56 -0500

    added person class for example

--- app/assets/javascripts/opals.js.rb
+++ app/assets/javascripts/opals.js.rb
@@ -1,5 +1,13 @@
+class Person
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+end
+
 Document.ready? do
   Element.find('.btn').on :click do
-    alert "A button was clicked!"
+    person = Person.new('Per')
+    alert "A button was clicked! by #{person.name}"
   end
 end
